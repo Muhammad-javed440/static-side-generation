520639fdcffa9c4c012ee923fc8fd22a81319c87
Muhammad-javed440 <meherjaved440@gmail.com>
2024-10-23 03:02:12 +0500

    add interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
-const getPostData = async()=>{
-  const response = await fetch("http://jsonplaceholder.typicode.com/posts",{
-    cache:'no-store',
-  })
-  return await response.json()
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
 }
 
+
+const getPostData = async () => {
+  const response = await fetch("http://jsonplaceholder.typicode.com/posts", {
+    cache: 'no-store',
+  });
+  return await response.json();
+};
+
 export default async function Home() {
-  const postData = await getPostData()
-  console.log(postData)
+  const postData = await getPostData();
+  console.log(postData);
+
   return (
-<div>
-  {postData.map((post:any) =>
-    <div><h1>{post.id}</h1><h1>Title : {post.title}</h1> <p>Description : {post.body}</p></div> )}
-</div>
+    <div>
+      {postData.map((post: Post) => (
+        <div key={post.id}>
+          <h1>Title: {post.title}</h1>
+          <p>Description: {post.body}</p>
+        </div>
+      ))}
+    </div>
   );
 }
+
